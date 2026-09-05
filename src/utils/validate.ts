90ce5355fd7ae065b5f19a8d25edad82a2755cc0
meluiz <me.luizfelipe@gmail.com>
2026-09-05 18:24:34 -0300

    feat(utils/validate.ts): add validation utility with zod schema support

---
+++ src/utils/validate.ts
@@ -0,0 +1,24 @@
+import type { ZodType } from 'zod';
+
+import { validator } from 'hono/validator';
+import z from 'zod';
+
+import { UnprocessableError } from './errors';
+
+type Target = 'query' | 'json' | 'param' | 'header';
+
+export const validate = <T extends ZodType>(target: Target, schema: T) => {
+  return validator(target, (value) => {
+    const result = schema.safeParse(value);
+
+    if (!result.success) {
+      const { fieldErrors } = z.flattenError(result.error);
+
+      throw new UnprocessableError(`The provided ${target} is invalid`, {
+        details: fieldErrors,
+      });
+    }
+
+    return result.data;
+  });
+};

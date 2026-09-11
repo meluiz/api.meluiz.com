6c678df95b82df212cfc5452950d1986bcc377a5
meluiz <me.luizfelipe@gmail.com>
2026-09-10 21:00:05 -0300

    refactor(plugins): remove unused compress plugin to simplify middleware stack and reduce unnecessary dependencies

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import type { ServerContext } from '#util/types';
 import { Hono } from 'hono';
 
 import { bodyLimit } from '#plugin/body-limit';
-import { compress } from '#plugin/compress';
 import { cors } from '#plugin/cors';
 import { onError, onNotFound } from '#plugin/error';
 import { logger } from '#plugin/logger';
@@ -20,7 +19,6 @@ hono.use('*', requestId);
 hono.use('*', cors);
 hono.use('*', rateLimit);
 hono.use('*', bodyLimit);
-hono.use('*', compress);
 hono.use('*', logger);
 
 hono.onError(onError);

247484a2510a091b0090db9c95a65ca8bc456d85
meluiz <me.luizfelipe@gmail.com>
2026-09-12 13:57:57 -0300

    feat(metadata): export additional types for better API usability

--- src/controllers/metadata/index.ts
+++ src/controllers/metadata/index.ts
@@ -2,17 +2,24 @@ export type { ExtractMetadataOptions } from './extractor';
 export type { FetchFaviconOptions } from './favicon';
 export type { FetchDocumentOptions } from './service';
 export type {
+  Alternate,
   Author,
   Crawler,
   Favicon,
   General,
   Metadata,
+  MetadataDocument,
   Mobile,
   Opengraph,
   OpengraphImage,
+  OpengraphMedia,
+  SiteVerification,
+  StructuredData,
+  StructuredDataIssue,
   ThemeColor,
   TouchIcon,
   Twitter,
+  TwitterLabel,
 } from './types';
 
 export { extractMetadata } from './extractor';
